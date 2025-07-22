97338b7c0f663dd17f557931c0cf88d84f60b464
Student <Student@Train11-3-PC190.control.ad.sit.kmutt.ac.th>
2025-07-22 16:15:41 +0700

    BasicP_class_2and Class_2 workshop.py

---
+++ BasicP_class_2.py
@@ -0,0 +1,54 @@
+# # x = "That's something"
+# # print("ค่าตัวแปร x:",x)
+# # y = 20.0
+# # b = False
+# # print("ค่าตัวแปร y:",y)
+# # print("Type ของ x:", type(x))
+# # print("Type ของ b:", type(b))
+# # print("Type ของ y:", type(y))
+# num = "นำ"
+# print("ค่าของ num: ", num)
+# print("Tpye ของ num: ", type(num))
+# print("หลังจากแปลงค่า") 
+# print("Type ของ num: ", type(float(num)))
+# mod = 10 % 4
+# print(mod)
+# ten = 10
+# one = 1
+# print()
+# T = True
+# F = False
+# print( not T or F)
+# num1 = int(input("กรอกตัวเลข: "))
+# num2 = int(input("กรอกตัวเลข: "))
+# print("num =", num1)
+# print("num =", num2)
+# print("num =", type(num1))
+# print("num =", type(num2))
+# print(num1 > num2)
+
+# x = 3
+# y = 5
+# if x == y:
+#     print("inisde if:", "x == y")
+# elif x > y :
+#     print("inside elif:", "x > y")
+# elif x < y:
+#     print("inside elif:", "x < y")
+# else:
+#     print("inside else:", "x != y")
+# print("outside if: ", "staterpack Yai Mak")
+
+# T = True
+# F = False
+# if T and F:
+#     print("inside if: False")
+# else:
+#     print("inside else: True")
+
+M = int(input("อายุ :"))
+
+if M < 10:
+    print("para")
+else:
+    print("E")
